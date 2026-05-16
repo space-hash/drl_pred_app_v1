c4fd93a325cb38a392c6cb84b060f8adb5292e86
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-05-17 00:08:57 +0530

    Add no-cache headers to responses and update mitigation rate limit configuration

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, make_response
 import os
 import sys
 import socket
@@ -18,6 +18,13 @@ from core.controller import (
 from capapp.config.settings import config
 
 app = Flask(__name__)
+
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 DEFAULT_DETECTION_LIMIT = 20
 MAX_DETECTION_LIMIT = 100
